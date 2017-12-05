0163a0615af5f0597315b4c378edfbd972d011b8
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-12-05 09:18:46 +0700

    @mecha-cms/genome/engine/kernel/union.php

--- normal/widget.form.search.php
+++ normal/widget.form.search.php
@@ -2,7 +2,7 @@
   <section class="widget-body">
     <div class="widget-content">
       <form class="form-search" action="<?php echo $url . '/' . $state->widget['page']['path']; ?>" method="get">
-        <?php echo Form::text($config->q, Request::get($config->q), $language->search . '…', ['classes' => ['input']]) . ' ' . Form::submit(null, null, "", ['classes' => ['button']]); ?>
+        <?php echo Form::text($config->q, Request::get($config->q), $language->search . '…', ['class[]' => ['input']]) . ' ' . Form::submit(null, null, "", ['class[]' => ['button']]); ?>
       </form>
     </div>
   </section>

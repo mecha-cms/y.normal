6b43983d8bbe71f78a4d29bde2609d1876b4660a
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-01-11 11:38:50 +0700

    Update

--- footer.php
+++ footer.php
@@ -1,16 +1,24 @@
 <footer class="footer">
   <div class="footer-left">
-    &#x00A9; <?= $time->year; ?> <a href="<?= $url; ?>"><?= $site->title; ?></a> &#x00B7; <?= i('Powered by %s', ['<a href="https://mecha-cms.com" target="_blank">Mecha ' . VERSION . '</a>']); ?>
+    &#x00A9; <?= $time->year; ?> <a href="<?= $url; ?>">
+      <?= $site->title; ?>
+    </a> &#x00B7; <?= i('Powered by %s', ['<a href="https://mecha-cms.com" target="_blank">Mecha ' . VERSION . '</a>']); ?>
   </div>
   <div class="footer-right">
-    <?php $path = State::get('x.user.path'); ?>
-    <?php if (State::get('x.user.guard.path')): ?>
-      <span class="a" title="<?= i('Log-in link has been disabled by the author.'); ?>"><?= i('Log In'); ?></span>
+    <?php $path = $state->x->user->path ?? '#'; ?>
+    <?php if (!empty($state->x->user->guard->path)): ?>
+      <span class="a" title="<?= i('Log-in link has been disabled by the author.'); ?>">
+        <?= i('Log In'); ?>
+      </span>
     <?php else: ?>
-      <?php if ($u = Is::user()): ?>
-      <a href="<?= $url . $path; ?>/<?= $user->name; ?>?exit=<?= $user->token; ?>"><?= i('Log Out'); ?></a>
+      <?php if ($site->is('enter')): ?>
+      <a href="<?= $url . $path; ?>/<?= $user->name; ?>?exit=<?= $user->token; ?>">
+        <?= i('Log Out'); ?>
+      </a>
       <?php else: ?>
-      <a href="<?= $url . $path; ?>"><?= i('Log In'); ?></a>
+      <a href="<?= $url . $path; ?>">
+        <?= i('Log In'); ?>
+      </a>
       <?php endif; ?>
     <?php endif; ?>
   </div>
